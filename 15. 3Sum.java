157b2d83b2ab175fc5634c8d1511515edc9af7a5
Shen Zhu <shenzhu0127@gmail.com>
2016-04-07 23:28:08 +0800

    fix bugs

--- 15. 3Sum.java
+++ 15. 3Sum.java
@@ -11,7 +11,10 @@ public class Solution {
                         list.add(nums[i]);
                         list.add(nums[j]);
                         list.add(nums[k]);
-                        resultList.add(list);
+
+                        if(!resultList.contains(list)){
+                            resultList.add(list);
+                        }
                     }
                 }
             }
@@ -22,35 +25,35 @@ public class Solution {
 
 /* Find Complement */
 public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
         
-        //sort array
-        java.util.Arrays.sort(nums);
+    //sort array
+    java.util.Arrays.sort(nums);
         
-        //find complement
-        int complement;
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                complement = 0 - nums[i] - nums[j];
+    //find complement
+    int complement;
+    for(int i = 0; i < nums.length; i++){
+        for(int j = i + 1; j < nums.length; j++){
+            complement = 0 - nums[i] - nums[j];
                 
-                int index = java.util.Arrays.binarySearch(nums, complement);
-                if(index >= 0 && nums[index] > nums[j]){
-                    List<Integer> list = new ArrayList<Integer>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[index]);
+            int index = java.util.Arrays.binarySearch(nums, complement);
+            if(index >= 0 && nums[index] > nums[j]){
+                List<Integer> list = new ArrayList<Integer>();
+                list.add(nums[i]);
+                list.add(nums[j]);
+                list.add(nums[index]);
                     
-                    if(!result.contains(list)){
-                        result.add(list);
-                    }
+                if(!result.contains(list)){
+                    result.add(list);
                 }
             }
         }
-        return result;
     }
+    return result;
+}
 
 
-/* Thid Method */
+/* Third Method */
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
