d28c67c1fd3013228b9a4e2a4a2822a6366b5043
Shen Zhu <shenzhu0127@gmail.com>
2016-07-05 12:06:01 +0800

    math, palindrome

---
+++ 9. Palindrome Number.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+    
+        if (x < 0) return false;
+
+        int p = x; 
+        int q = 0; 
+    
+        while (p >= 10){
+            q *=10; 
+            q += p%10; 
+            p /=10; 
+        }
+    
+        return q == x / 10 && p == x % 10;
+    }
+}
