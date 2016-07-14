d5b1f44d741342c75f246aa826704c3442c4e538
Shen Zhu <shenzhu0127@gmail.com>
2016-07-14 10:33:33 +0800

    dynamic programming

---
+++ 152. Maximum Product Subarray.java
@@ -0,0 +1,18 @@
+public class Solution{
+    public int maxProduct(int[] nums){
+        if (nums == null || nums.length == 0) return 0;
+
+        int max = nums[0], min = nums[0], result = nums[0];
+
+        // DP
+        for (int i = 1; i < nums.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+            if (max > result) {
+                result = max;
+            }
+        }
+        return result;
+    }
+}
