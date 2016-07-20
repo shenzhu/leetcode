98703047acefdb941757937be8ea91589f68e7a3
Shen Zhu <shenzhu0127@gmail.com>
2016-07-20 22:54:22 +0800

    math, string, stringbuilder

---
+++ 168. Excel Sheet Column Title.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        while(n > 0){
+            n--;        // adjust to index
+            sb.insert(0, (char)('A' + n % 26));
+            n /= 26;
+        }
+        
+        return sb.toString();
+    }
+}
