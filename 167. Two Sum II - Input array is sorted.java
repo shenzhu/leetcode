4b9b27c3a2a8ca17accbe5cea8c6e999708cf930
Shen Zhu <shenzhu0127@gmail.com>
2016-09-09 16:37:25 -0400

    Array, Two pointers

---
+++ 167. Two Sum II - Input array is sorted.java
@@ -0,0 +1,11 @@
+public int[] twoSum(int[] numbers, int target){
+	int start = 0;
+	int end = numbers.length - 1;
+
+	while(start < end){
+		if(numbers[start] + numbers[end] == target) break;
+		if(numbers[start] + numbers[end] < target) start++;
+		else end--;
+	}
+	return new int[] {start + 1, end + 1};
+}
